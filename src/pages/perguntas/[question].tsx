43bb7c19a9680e4cb1392d1eacc6d3a49006f5ed
4ss1s <assisguitar23@gmail.com>
2023-07-28 20:44:14 -0300

    busca por graphql

--- src/pages/perguntas/[question].tsx
+++ src/pages/perguntas/[question].tsx
@@ -1,12 +1,26 @@
 import { GetServerSideProps } from 'next'
 import { useForm } from 'react-hook-form'
+import { gql, useQuery } from '@apollo/client'
 
 import { axios } from '@/lib'
+import { useEffect } from 'react'
 
 const Question = ({ repo }: { repo: any }) => {
 	// if (repo.err) {
 	// 	return <>Erro ao tentar encontrar o questionar</>
 	// }
+
+	const { data, error } = useQuery(gql`
+		query Question {
+			questions {
+				id
+			}
+		}
+	`)
+
+	useEffect(() => {
+		console.log({ data, error })
+	}, [data, error])
 
 	const {
 		register,
@@ -44,18 +58,18 @@ const Question = ({ repo }: { repo: any }) => {
 
 export default Question
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-	const { question } = params as any
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+// 	const { question } = params as any
 
-	const repo = await fetch(
-		`http://localhost:3000/api/question/${question}`
-	).then(res => res.json())
+// 	const repo = await fetch(
+// 		`http://localhost:3000/api/question/${question}`
+// 	).then(res => res.json())
 
-	console.log({ repo })
+// 	console.log({ repo })
 
-	return {
-		props: {
-			repo
-		}
-	}
-}
+// 	return {
+// 		props: {
+// 			repo
+// 		}
+// 	}
+// }
